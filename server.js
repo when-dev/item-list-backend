0bea602f9841000dcbddc1a76e64d9507a2099c7
Egor Gukov <108114245+when-dev@users.noreply.github.com>
2025-04-26 16:15:12 +0300

    backend part

---
+++ server.js
@@ -0,0 +1,78 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const PORT = 5000
+
+app.use(cors())
+app.use(express.json())
+
+const ITEMS_COUNT = 1_000_000
+
+let items = Array.from({ length: ITEMS_COUNT }, (_, i) => ({
+	id: i + 1,
+	selected: false,
+}))
+
+let orderedItems = items.map(item => item.id)
+
+app.get('/api/items', (req, res) => {
+	let { search = '', offset = 0, limit = 20 } = req.query
+
+	const searchLower = search.toLowerCase()
+
+	let filteredIds
+	if (!search.trim()) {
+		filteredIds = orderedItems
+	} else {
+		filteredIds = orderedItems.filter(id => `item #${id}`.includes(searchLower))
+	}
+
+	const paginatedIds = filteredIds.slice(+offset, +offset + +limit)
+
+	res.json({
+		items: paginatedIds.map(id => ({
+			id,
+			selected: items[id - 1].selected,
+		})),
+		total: filteredIds.length,
+	})
+})
+
+app.post('/api/items/order', (req, res) => {
+	const { orderedIds } = req.body
+
+	const newOrdered = []
+
+	let used = new Set(orderedIds)
+
+	newOrdered.push(...orderedIds)
+
+	orderedItems.forEach(id => {
+		if (!used.has(id)) {
+			newOrdered.push(id)
+		}
+	})
+
+	orderedItems = newOrdered
+
+	res.status(200).send()
+})
+
+app.get('/api/selected', (req, res) => {
+	const selectedItems = items
+		.filter(item => item.selected)
+		.map(item => ({
+			id: item.id,
+			selected: item.selected,
+		}))
+
+	res.json(selectedItems)
+})
+
+app.post('/api/items/select', (req, res) => {
+	const { id, selected } = req.body
+	items[id - 1].selected = selected
+	res.status(200).send()
+})
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
